825e5acf9f6e4299e55bf28a681da986cf5ff443
VitorT <vitortakayuki@gmail.com>
2024-09-05 22:32:13 -0300

    05-09

--- array.php
+++ array.php
@@ -57,8 +57,71 @@
 
     <div class="container">
 
-    <h1>Pagina inicial do site</h1>
+    <h1>Array</h1>
 
+    <p>As variaveis tipo array são muito utilizadas quando vamos listar coisas. Os arrays em php são listas de quaisquer tipo de dados.</p>
+
+    <pre>
+        $array =[];
+        $array = Array ();
+        $array[0] = "OI";
+        $array[2] = 10;
+        $array ["Vitor"] = "Aluno";
+        $array[$array[2]] = "teste";
+
+    </pre>
+    <?php
+
+    $array = Array("oi",10,"prof","teste",1.99, true);
+    echo "<h2>Como saber se deu certo?</h2>";
+    var_dump($array);
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+
+    echo "<pre>";
+    var_dump($array);
+    echo "</pre>";
+    ?>
+    <p>Existem varias formas de crirar uma variavel array</p>
+        <pre>
+            array(
+                chave => valor,
+                chave2 => valor2,
+                chave3 => valor3,
+                ...
+            )
+        </pre>
+        <p>Esse tipo </p>
+
+        <?php
+            $array = array(
+            "foo" => "bar",
+            "bar" => "foo",
+            );
+
+            //Utilizando sintaxe curta
+            $array = [
+            "foo" => "bar",
+            "bar" => "foo",
+            ];
+            echo "<pre>";
+            print_r($array);
+            echo "</pre>";
+
+            $resultados = [
+                "Id" => 1,
+                "Nome" => "Fernando",
+                "Idade" => 34,
+                "Salario" => 5325.50,
+                "Professor" => true
+            ];
+            echo "<pre>";
+            print_r($resultados);
+            echo "</pre>";
+
+            echo $resultados["Nome"];
+        ?>
     </div>
     
 
